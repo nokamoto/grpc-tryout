608c07b6657a40143c213fdb37a8e20b518093f8
Naofumi Okamoto <nokamoto.engr@gmail.com>
2023-10-30 12:54:30 +0000

    example

--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -2,8 +2,11 @@ import "./App.css";
 import { ConnectError, createPromiseClient } from "@connectrpc/connect";
 import { createConnectTransport } from "@connectrpc/connect-web";
 import { Library } from "./gen/apis/example/service_connect";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { Shelf } from "./gen/apis/example/resource_pb";
+import doc from "./gen/example/apis/example/service.pb.json";
+import { Method, Proto, Service } from "./gen/apis/tryout/tryout_pb";
+import { GetShelfRequest } from "./gen/apis/example/service_pb";
 
 const baseUrl = document.location.href.replace("5173", "8080");
 
@@ -13,17 +16,41 @@ const transport = createConnectTransport({
 
 const client = createPromiseClient(Library, transport);
 
-function App() {
-  const [inputValue, setInputValue] = useState("");
+function createField(
+  field: string,
+  value: GetShelfRequest,
+  set: React.Dispatch<SetStateAction<GetShelfRequest>>,
+) {
+  return (
+    <label>
+      {field}
+      <input
+        value={value.name}
+        onChange={(e) => {
+          const v = value.clone();
+          v.name = e.target.value;
+          set(v);
+        }}
+      />
+    </label>
+  );
+}
+
+function createMethod(method: Method) {
+  const [inputValue, setInputValue] = useState(new GetShelfRequest());
   const [shelf, setShelf] = useState<Shelf | null>(null);
   const [error, setError] = useState<ConnectError | null>(null);
+
   return (
-    <>
+    <div>
+      <h3>{method.name}</h3>
+      {shelf && <p>shelf = {shelf.toJsonString()}</p>}
+      {error && <p>error = {error.message}</p>}
       <form
         onSubmit={async (e) => {
           e.preventDefault();
           try {
-            const response = await client.getShelf({ name: inputValue });
+            const response = await client.getShelf(inputValue);
             setShelf(response);
           } catch (err) {
             const connectErr = ConnectError.from(err);
@@ -31,17 +58,28 @@ function App() {
           }
         }}
       >
-        <p>{baseUrl}</p>
-        {shelf && <p>shelf = {shelf.toJsonString()}</p>}
-        {error && <p>error = {error.message}</p>}
-        <input
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-        />
+        <label>
+          {method.fields.map((field) =>
+            createField(field, inputValue, setInputValue),
+          )}
+        </label>
         <button type="submit">Send</button>
       </form>
-    </>
+    </div>
   );
+}
+
+function createService(service: Service) {
+  return (
+    <div>
+      <h2>{service.name}</h2>
+      <div>{service.methods.map(createMethod)}</div>
+    </div>
+  );
+}
+
+function App() {
+  return <div>{Proto.fromJson(doc).services.map(createService)}</div>;
 }
 
 export default App;
